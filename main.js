cc1e5741bc85da9d1b8618d7cc120a8ed56f91bd
Dloft <derek.lofton1@gmail.com>
2021-03-10 22:52:09 -0500

    display question

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 
+
+
 function GameStart() {
     console.log("I was clicked")
+    //showing the game
     document.getElementById("game-container").style.display = "block";
+    //start button gone
+    document.getElementById("start-btn").style.display = "none";
+   //put the question from array
+   document.getElementById("theQuestion").innerHTML = questions[0].question;
+   console.log(questions[0].question);
 }
 
 function NextQuestion() {
@@ -11,3 +19,20 @@ function NextQuestion() {
 function Submit() {
 
 }
+
+var questions = 
+      [
+        {
+          question: "What is the capital of United Kingdom?",
+          choices: ["Manchester", "Birmingham", "London", "Birmingham"],
+          answer: 2
+        },
+        
+        {
+          question: "What is the capital of United States?",
+          choices: ["California", "New York", "Miami", "Florida"],
+          answer: 1
+        }
+        
+        
+      ];
