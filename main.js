56d482169f1cd4fe06c8b16350c11a8c4122b003
Dloft <derek.lofton1@gmail.com>
2021-03-10 22:32:36 -0500

    gamestart

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function GameStart() {
+    console.log("I was clicked")
+    document.getElementById("game-container").style.display = "block";
+}
+
+function NextQuestion() {
+
+}
+
+function Submit() {
+
+}
